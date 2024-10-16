92e50235e93bd72b1fdeb595ab1bfd0b07733322
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2024-10-16 11:53:22 +0100

    added javascript class

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+console.log("Hello world!")
+console.log("Sanni Damilola")
+console.log(300)
+console.log(10.5)
+console.log(true)
+console.log(typeof "Hello")
+
+// this is a comment
+
+console.log("hey" == "HEY")
+console.log("hey" == "hey")
+
+// variables
+
+// let
+let myName = "Esther"
+let number = 30
+// console.log(myName)
+console.log(number)
+
+myName = "Favour"
+console.log(myName)
+
+// var
+var name = "Chinedu"
+console.log(name)
+var name = "Sanni"
+console.log(name)
+
+// const
+const age = 50
+console.log(age)
