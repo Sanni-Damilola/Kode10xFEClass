8e6d98a5d6d5f48c0ef74f71d992d0d34704c423
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2024-10-18 11:57:50 +0100

    added string methods

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,52 @@
-console.log("Hello world!")
-console.log("Sanni Damilola")
-console.log(300)
-console.log(10.5)
-console.log(true)
-console.log(typeof "Hello")
+// console.log("Hello world!")
+// console.log("Sanni Damilola")
+// console.log(300)
+// console.log(10.5)
+// console.log(true)
+// console.log(typeof "Hello")
 
-// this is a comment
+// // this is a comment
 
-console.log("hey" == "HEY")
-console.log("hey" == "hey")
+// console.log("hey" == "HEY")
+// console.log("hey" == "hey")
 
-// variables
+// variables /data types
 
 // let
-let myName = "Esther"
-let number = 30
+let myName = "Esther" // string
+let number = 30 // number
 // console.log(myName)
 console.log(number)
 
-myName = "Favour"
+myName = "Favour" // string
 console.log(myName)
 
 // var
-var name = "Chinedu"
+var name = "Chinedu" // string
 console.log(name)
-var name = "Sanni"
+var name = "Sanni" // string
 console.log(name)
 
 // const
-const age = 50
+const age = 50 /// number
 console.log(age)
+
+// boolean
+const isPresent = false // boolean
+const isMale = true // boolean
+console.log(isPresent)
+console.log(isMale)
+
+// null
+let myAccountBalance = null // null
+console.log(myAccountBalance)
+
+myAccountBalance = 1000 // numer
+console.log(myAccountBalance)
+
+// underfined
+let tomorrowTask // underfined
+console.log(tomorrowTask)
+
+tomorrowTask = "Clean The Table" // string
+console.log(tomorrowTask)
---
+++ javascript/string.js
@@ -0,0 +1,42 @@
+let text = 'We\'re so-Called "Vikings" From the north.'
+// console.log(text)
+let family = "Jesus"
+// console.log(family)
+
+// String Methods
+
+// 1: Length
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+console.log(alphabet.length)
+
+let muttipleNumbers = "123456789".length
+console.log(muttipleNumbers)
+let space = " "
+console.log(space.length)
+
+// chatAt()
+let greet = "Hello World"
+console.log(greet.charAt(1))
+
+// charCodeAt
+let text1 = "How Are you?"
+console.log(text1.charCodeAt(1))
+
+// at
+let name = "Hello World"
+console.log(name.at(1)) // outdated
+
+// toUpperCase()
+let userName = "sanni damilola"
+console.log(userName.toUpperCase())
+
+// toLowerCase()
+let myName = "JOHN"
+console.log(myName.toLowerCase())
+
+// index of
+let alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+console.log(alphabet2.indexOf("a"))
+
+// includes
+console.log(alphabet2.includes("a"))
